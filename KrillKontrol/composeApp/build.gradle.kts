a6cfa5b2f6da6d939bf118d5ec34171e30abf131
Benjamin Sautner <bsautner@gmail.com>
2025-06-18 16:42:14 -0400

    ios

--- KrillKontrol/composeApp/build.gradle.kts
+++ KrillKontrol/composeApp/build.gradle.kts
@@ -97,9 +97,9 @@ kotlin {
             dependsOn(commonMain)
             // any iOS-only Kotlin libs here
         }
-//        getByName("iosX64Main").dependsOn(iosMain)
-//        getByName("iosSimulatorArm64Main").dependsOn(iosMain)
-//        getByName("iosArm64Main").dependsOn(iosMain)
+        getByName("iosX64Main").dependsOn(iosMain)
+        getByName("iosSimulatorArm64Main").dependsOn(iosMain)
+        getByName("iosArm64Main").dependsOn(iosMain)
 
 
 
